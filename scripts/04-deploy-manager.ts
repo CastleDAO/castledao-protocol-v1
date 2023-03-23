999c27e6006a8c757092e9dabd669d4e8ed14aa9
Rafael Ventura <rafinskipg@gmail.com>
2023-03-23 09:22:18 +0100

    deploy master

--- scripts/04-deploy-manager.ts
+++ scripts/04-deploy-manager.ts
@@ -14,26 +14,40 @@ async function main() {
   console.log(`Network: ${networkName} (chainId=${network.chainId})`);
   console.log("Manager contract deployed to:", manager.address);
 
-  // Add managers
-  const manager1Address = "0xd6ecFf5F50c1A7e27C2e0422C76817FD21c44b2D"; // Replace with the desired address
+  // Add admin:
+  if (network.chainId === 42161) {
+    // Admin multisig in mainnet
+    await manager.addAdmin('0xeEfC874aC40BCF8A00b4484F26F599d0CE6c0F47');
 
-  await manager.addManager(manager1Address, 0);
-  console.log(`Added manager ${manager1Address} with type 0`);
+    //Log 
+    console.log("Added admin multisig in mainnet");
 
-  await manager.addManager(manager1Address, 1);
-  console.log(`Added manager ${manager1Address} with type 1`);
+    // add manager 0
+    await manager.addManager('0x846FF49d72F4e3CA7a3D318820C6C2debe23c68A', 0);
+  } else {
 
-  await manager.addManager(manager1Address, 2);
-  console.log(`Added manager ${manager1Address} with type 2`);
+    // Add managers
+    const manager1Address = "0xd6ecFf5F50c1A7e27C2e0422C76817FD21c44b2D"; // Replace with the desired address
+  
+    await manager.addManager(manager1Address, 0);
+    console.log(`Added manager ${manager1Address} with type 0`);
+  
+    await manager.addManager(manager1Address, 1);
+    console.log(`Added manager ${manager1Address} with type 1`);
+  
+    await manager.addManager(manager1Address, 2);
+    console.log(`Added manager ${manager1Address} with type 2`);
+  
+    await manager.addManager(manager1Address, 3);
+    console.log(`Added manager ${manager1Address} with type3`);
+  
+    await manager.addManager(manager1Address, 4);
+    console.log(`Added manager ${manager1Address} with type 4`);
+  
+    await manager.addManager(manager1Address, 5);
+    console.log(`Added manager ${manager1Address} with type 5`);
+  }
 
-  await manager.addManager(manager1Address, 3);
-  console.log(`Added manager ${manager1Address} with type3`);
-
-  await manager.addManager(manager1Address, 4);
-  console.log(`Added manager ${manager1Address} with type 4`);
-
-  await manager.addManager(manager1Address, 5);
-  console.log(`Added manager ${manager1Address} with type 5`);
 
 
   if (networkName != "localhost") {
--- scripts/constants.ts
+++ scripts/constants.ts
@@ -13,7 +13,7 @@ export const deployed_contracts = {
         generals: '0x284d29b06CDF38043cca6FD7b229739e377523E3',
         ballot: '0xA86F02D8854e6c1dB41eB3308138D0473250CAF5',
         manager: '0xb41a1D1fCff8b00d1342881561BFa3Edb495f748',
-        masterCastles: '0x385f3036C00FCAEc2583D73Ef8ef244EEC38DF7E',
+        masterCastles: '0x474D5fBafda609670fBe96D92dcd90Ec5F02b9B4',
 
         // Other
         magic: '0x2D110bF8fd17b514CbdCa025a97AAeb5E463AF4E',
@@ -29,8 +29,8 @@ export const deployed_contracts = {
         generals: '0x1aaec0fa487a979a3f6b46dccf0ac2648167a61e',
         ballot: '0x9fe6688e7d4bfbc69fe2727f578b1f1b8c75b930',
         defiheroes: '0x8ec75bc963181489d7fc1d892f687b8b0987d9ec',
-        manager: '',
-        masterCastles: '',
+        manager: '0x8937fF68da18e97B58109b598a6f0EbbF82DA3E2',
+        masterCastles: '0xfc7590e2d9226327FF9D517F2D3653C05b7f625E',
 
         // Other
         magic: '0x539bde0d7dbd336b79148aa742883198bbf60342',
